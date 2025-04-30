7d81f75e353ca0be1f0d1c56f664b1d0868adec0
fernando-cb12 <fernandocamoub@gmail.com>
2025-04-30 13:30:06 -0700

    feat: mod header

--- src/components/headerContent.tsx
+++ src/components/headerContent.tsx
@@ -2,10 +2,10 @@ import { UserIcon } from "@heroicons/react/16/solid";
 //this component is a header that containts the title for each section
 export default function HeaderContent({ title }: { title: string }) {
   return (
-    <div className="flex flex-col items-center justify-center bg-blue-100 w-full">
-      <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
-      <div className="flex items-center mt-2">
-        <UserIcon />
+    <div className="flex flex-col w-full bg-[#F2F0EB] p-4 shadow-md">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
+        <UserIcon className="h-8 w-8 text-gray-800" />
       </div>
     </div>
   );
